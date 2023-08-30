7bab0edfa02ddf464c68f1928611dd323025e18b
Yogesh Aaryaveer <86904855+theaaryaveer@users.noreply.github.com>
2023-08-30 14:10:13 +0545

    Update search2Darray.java

--- search2Darray.java
+++ search2Darray.java
@@ -20,6 +20,7 @@ public class search2Darray {
         Scanner sc = new Scanner(System.in);
         for(int i=0; i<n;i++){
             for(int j=0; j<m;j++){
+             
                 matrix[i][j]=sc.nextInt();
             }
         }
