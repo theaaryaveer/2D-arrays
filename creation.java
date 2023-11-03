f312b2495f65b2675ceb95de60e657593ee76616
Yogesh Aaryaveer <86904855+theaaryaveer@users.noreply.github.com>
2023-11-03 16:04:15 +0545

    Update creation.java

--- creation.java
+++ creation.java
@@ -14,7 +14,7 @@ public class creation{
         //output
          for(int i=0; i<n;i++){
             for(int j=0; j<m;j++){
-                System.out.print(matrix[i][j]+ " ");
+                System.out.print(matrix[i][j]+ " "); 
                 
              
             }
